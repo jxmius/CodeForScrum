9e12f1f817c79381d6b36f92f56a2da36d296d1c
scottbpgit config --global user.name scottbp <spric20020@gmail.com>
2023-10-18 17:02:08 -0400

    Implemented FileReaderAndWriter class #76 on board

---
+++ FileReaderAndWriter.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class FileReaderAndWriter {
+    private String fileName;
+    protected String line;
+
+    public String getFileName(Project project) {
+        //Implement
+        return null;
+    }
+
+    public void addToFile(String content) {
+        //Implement
+    }
+
+    public void removeFromFile(String contentToRemove) {
+        //Implement
+    }
+
+    public void editFile() {
+        // Implement
+    }
+
+    public void readFile() {
+        // Implement
+    }
+
+    public void removeFile() {
+        // Implement
+    }
+
+    public void search(String query) {
+        // Implement
+    }
+}
