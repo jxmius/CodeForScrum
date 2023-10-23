ef93202b6c90f44963ae044988552800aa0aa00b
Jamius <jamius0414@sc.edu>
2023-10-23 13:50:03 -0400

    json

---
+++ DataLoader.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DataLoader {
+    private String projectFilePath;
+    private String userFilePath;
+
+    public DataLoader(String projectFilePath, String userFilePath) {
+        this.projectFilePath = projectFilePath;
+        this.userFilePath = userFilePath;
+    }
+
+    public ArrayList<Project> getProjects() {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(projectFilePath)) {
+            return (ArrayList<Project>) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    public ArrayList<User> getUsers() {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(userFilePath)) {
+            return (ArrayList<User>) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+}
