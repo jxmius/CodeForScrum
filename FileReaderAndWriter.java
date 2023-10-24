fef4c55f241ea06257a0cf239d209ea3eac57a05
scottbpgit config --global user.name scottbp <spric20020@gmail.com>
2023-10-24 12:17:06 -0400

    Ammended FileReaderAndWriter class loadProject

--- FileReaderAndWriter.java
+++ FileReaderAndWriter.java
@@ -1,5 +1,6 @@
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -38,27 +39,40 @@ public class FileReaderAndWriter {
     }
 
     public static ArrayList<Project> loadProjects() {
-        ArrayList<Project> projects = new ArrayList<>();
+    ArrayList<Project> projects = new ArrayList<>();
 
-        try {
-            FileReader reader = new FileReader("src/project.json");
-            JSONArray projectsJSON = (JSONArray) (new JSONParser()).parse(reader);
+    try {
+        FileReader reader = new FileReader("src/project.json");
+        JSONArray projectsJSON = (JSONArray) (new JSONParser()).parse(reader);
 
-            for (Object obj : projectsJSON) {
-                JSONObject projectJSON = (JSONObject) obj;
+        for (Object obj : projectsJSON) {
+            JSONObject projectJSON = (JSONObject) obj;
+
+            String id = (String) projectJSON.get("id");
+            String projectName = (String) projectJSON.get("projectName");
+            String board = (String) projectJSON.get("board");
+            String dateTime = (String) projectJSON.get("dateTime");
+            
+            ArrayList<Contributor> contributors = new ArrayList<>();
+            JSONArray contributorsJSON = (JSONArray) projectJSON.get("contributors");
+            for (Object contributorObj : contributorsJSON) {
+                JSONObject contributorJSON = (JSONObject) contributorObj;
+                String username = (String) contributorJSON.get("username");
+                String firstName = (String) contributorJSON.get("firstName");
+                String lastName = (String) contributorJSON.get("lastName");
                 
-                String id = (String) projectJSON.get("id");
-                String projectName = (String) projectJSON.get("projectName");
-                String board = (String) projectJSON.get("board");
-                String dateTime = (String) projectJSON.get("dateTime");
-                Project project = new Project(id, projectName, board, dateTime, /*... additional args ... */);
-                projects.add(project);
+                Contributor contributor = new Contributor(username, firstName, lastName);
+                contributors.add(contributor);
             }
-
-            return projects;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            Project project = new Project(id, projectName, board, dateTime);
+            projects.add(project);
         }
+
+        return projects;
+    } catch (Exception e) {
+        e.printStackTrace();
+        return null;
     }
+}
+
 }
