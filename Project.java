313e871b9f2da2805c8951bf338fa7f6d0c0b186
Jamius <jamius0414@sc.edu>
2023-10-24 13:53:56 -0400

    Project

--- Project.java
+++ Project.java
@@ -1,81 +1,46 @@
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class Project {
-    private UUID id;
+    private String id;
     private String projectName;
-    private Board board;
-    private String projectDate;
-    private String projectTime;
-    private ArrayList<String> comments;
-    private ArrayList<String> contributorList;
-    private String projectDescription;
+    private String board;
+    private String dateTime;
+    private ArrayList<Comment> comments;
+    private ArrayList<Contributor> contributors;
 
-    public Project(UUID id, String projectName) {
+    public Project(String id, String projectName, String board, String dateTime) {
         this.id = id;
         this.projectName = projectName;
+        this.board = board;
+        this.dateTime = dateTime;
         this.comments = new ArrayList<>();
-        this.contributorList = new ArrayList<>();
-        this.projectDescription = "";
+        this.contributors = new ArrayList<>();
     }
 
-    public void setDateTime(String date, String time) {
-        this.projectDate = date;
-        this.projectTime = time;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public String getBoard() {
+        return board;
     }
 
     public String getDateTime() {
-        return projectDate + " " + projectTime;
+        return dateTime;
     }
 
-    public void displayProjectName(String name) {
-        System.out.println("Project Name: " + name);
+    public ArrayList<Comment> getComments() {
+        return comments;
     }
 
-    public void editProjectDescription(String newDescription) {
-        this.projectDescription = newDescription;
-    }
-
-    public String displayProject() {
-        return "Project ID: " + id +
-                "\nProject Name: " + projectName +
-                "\nProject Date: " + projectDate +
-                "\nProject Time: " + projectTime +
-                "\nProject Description: " + projectDescription;
-    }
-
-    public String displayProjectDate() {
-        return "Project Date: " + projectDate;
-    }
-
-    public void callBoard() {
-        // Implementation for calling the associated board
-    }
-
-    public void checkContributor() {
-        // Implementation for checking contributors
-    }
-
-    public void addComment(String message) {
-        comments.add(message);
-    }
-
-    public void removeComment(String message) {
-        comments.remove(message);
-    }
-
-    public void editComment(String oldMessage, String newMessage) {
-        int index = comments.indexOf(oldMessage);
-        if (index != -1) {
-            comments.set(index, newMessage);
-        }
-    }
-
-    public void addContributor(String contributor) {
-        contributorList.add(contributor);
-    }
-
-    public void deleteContributor(String contributor) {
-        contributorList.remove(contributor);
+    public ArrayList<Contributor> getContributors() {
+        return contributors;
     }
 }
+
+
