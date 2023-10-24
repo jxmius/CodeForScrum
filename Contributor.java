928e11a0f0e4614843132b3ecbb7b93f82bb901c
Jamius <jamius0414@sc.edu>
2023-10-24 13:56:14 -0400

    Contributor

--- Contributor.java
+++ Contributor.java
@@ -1,11 +1,23 @@
 public class Contributor {
     private String username;
+    private String firstName;
+    private String lastName;
 
-    public Contributor(String username) {
+    public Contributor(String username, String firstName, String lastName) {
         this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
