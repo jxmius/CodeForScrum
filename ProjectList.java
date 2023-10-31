7eac2a45655ab52664e894a4c202f709fd85b56d
ZachFlores <zjflores@email.sc.edu>
2023-10-31 13:59:17 -0400

    Adding projectlist class

--- ProjectList.java
+++ ProjectList.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjectList {
-    
+    private static ProjectList instance;
+    private List<Project> myProjects;
+
+    private ProjectList() {
+        myProjects = new ArrayList<>();
+    }
+
+    public static ProjectList getInstance() {
+        if (instance == null) {
+            instance = new ProjectList();
+        }
+        return instance;
+    }
+
+    public void addProject(Project project) {
+        myProjects.add(project);
+    }
+
+    public Project getProject(String keyword) {
+        for (Project project : myProjects) {
+            if (project.getKeyword().equals(keyword)) {
+                return project;
+            }
+        }
+        return null; // Project not found
+    }
 }
