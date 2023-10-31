030bc3e46dfd3bae49e023a65376d467609bbc55
scottbpgit config --global user.name scottbp <spric20020@gmail.com>
2023-10-31 18:19:43 -0400

    Added back functions to project.java

--- Comments.java
+++ Comments.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class Comments {
-    private String username;
+    private String username; 
+    private String message;
     private ArrayList<String> commentsArray;
 
     public Comments(String username) {
@@ -10,41 +11,22 @@ public class Comments {
     }
 
     public void comment(String comment, Contributor contributor) {
-        String commentText = username + " (" + contributor.getUsername() + "): " + comment;
-        commentsArray.add(commentText);
+        this.commentsArray.add(contributor.getFullName() + ": " + comment);
     }
 
-    public void addComment(String comment) {
-        commentsArray.add(comment);
+    public void addComments() {
+        this.commentsArray.add(username + ": " + message);
     }
 
-    public String getMessage(int commentIndex) {
-        if (commentIndex >= 0 && commentIndex < commentsArray.size()) {
-            return commentsArray.get(commentIndex);
-        } else {
-            return "Invalid comment index.";
-        }
+    public String getMessage() {
+        return message;
     }
 
-    public void displayComments() {
-        System.out.println("Comments:");
-        for (String comment : commentsArray) {
-            System.out.println(comment);
-        }
+    public void setMessage(String message) {
+        this.message = message;
     }
 
-    public static void main(String[] args) {
-        Contributor contributor = new Contributor("Alice", "Alice", "Smith");
-        Comments comments = new Comments("Bob");
-
-        comments.comment("This is a great idea!", contributor);
-        comments.comment("I agree with Alice.", contributor);
-        comments.addComment("Thanks for the input!");
-
-        comments.displayComments();
-
-        // Example of retrieving a comment message
-        String message = comments.getMessage(0);
-        System.out.println("Retrieved Comment: " + message);
+    public ArrayList<String> getCommentsArray() {
+        return commentsArray;
     }
 }
--- Project.java
+++ Project.java
@@ -5,7 +5,7 @@ public class Project {
     private String projectName;
     private String board;
     private String dateTime;
-    private ArrayList<Comment> comments;
+    private ArrayList<Comments> comments;
     private ArrayList<Contributor> contributors;
 
     public Project(String id, String projectName, String board, String dateTime) {
@@ -34,13 +34,44 @@ public class Project {
         return dateTime;
     }
 
-    public ArrayList<Comment> getComments() {
+    public ArrayList<Comments> getComments() {
         return comments;
     }
 
     public ArrayList<Contributor> getContributors() {
         return contributors;
     }
-}
 
+    public void addComment(Comments comment) {
+        this.comments.add(comment);
+    }
+
+    public void addContributor(Contributor contributor) {
+        this.contributors.add(contributor);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public void setBoard(String board) {
+        this.board = board;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+    
+    public void setComments(ArrayList<Comments> comments) {
+        this.comments = comments;
+    }
+
+    public void setContributors(ArrayList<Contributor> contributors) {
+        this.contributors = contributors;
+    }
+}
 
