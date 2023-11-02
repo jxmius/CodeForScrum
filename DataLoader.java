ccc6546d838d5e39242f609cb1148b196b500e3f
Jamius <jamius0414@sc.edu>
2023-11-02 14:09:20 -0400

    Updated DataLoader

--- DataLoader.java
+++ DataLoader.java
@@ -1,6 +1,6 @@
 import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class DataLoader {
